0f044a12afbbfecb416ef75d3fde3ea808280bde
Damir Sijakovic <50234352+damir-sijakovic@users.noreply.github.com>
2019-10-02 12:26:16 +0200

    Update sandyfox.sh

--- scripts/sandyfox.sh
+++ scripts/sandyfox.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# jails firefox; install firejail, make dir ~/Desktop/firejails/firefox/, run
 firejail --seccomp --caps.drop=all --noroot --private=~/Desktop/firejails/firefox/ firefox 
